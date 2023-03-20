fbd1c64a223eea2b3a3db5d296d1c56f2b147d18
이호영 <win_dy1207@naver.com>
2023-03-20 19:22:28 +0900

    master

---
+++ class/03-javascript/연습-07-Math.js
@@ -0,0 +1,28 @@
+Math.floor(Math.random() * 1000000) // 랜덤으로 6자리 숫자 생성
+// 820497
+String(Math.floor(Math.random() * 1000000)) // 문자열로 생성
+// '214462'
+String(Math.floor(Math.random() * 1000000))
+// '651511'
+String(Math.floor(Math.random() * 1000000))
+// '149859'
+String(Math.floor(Math.random() * 1000000))
+// '801577'
+String(Math.floor(Math.random() * 1000000))
+// '263226'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0") // 앞자리가 0으로 시작 가능
+// '191345'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '146344'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '128502'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '910585'
+String(Math.floor(Math.random() * 1000000)).padStart(6,"0")
+// '082804'
+let result = String(Math.floor(Math.random() * 1000000)).padStart(6,"0") //result값에 랜덤으로 생성된 숫자 넣기
+// undefined
+result
+// '134836'
+result
+// '134836'
