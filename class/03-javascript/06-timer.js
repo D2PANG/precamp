728641883f1a53af39d34452bc9b8db589c3ce14
이호영 <win_dy1207@naver.com>
2023-03-26 19:11:14 +0900

    master

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,19 +1,33 @@
+let isStarted = false;
+
 let math= () => {
-    const token = String(Math.floor(Math.random()*1000000)).padStart(6,"0")
-    document.getElementById("target").innerText = token
-    document.getElementById("target").style.color = "#" + token
+    if(isStarted === false){
+        //타이머가 작동중이 아닐 때
+        isStarted = true
+        document.getElementById("finish").disabled = false
+        const token = String(Math.floor(Math.random()*1000000)).padStart(6,"0")
+        document.getElementById("target").innerText = token
+        document.getElementById("target").style.color = "#" + token
+    
+        let time = 180
+        let timer
+    
+        timer = setInterval(function(){
+            if(time >= 0){
+                let min = Math.floor(time / 60)
+                let sec = String(time % 60).padStart(2, "0")
+                document.getElementById("timer").innerText = min + ":" + sec
+                time = time - 1
+            }
+            else {
+                document.getElementById("finish").disabled = True
+                isStarted = false
+                console.log("타이머 작동중")
+                clearInterval(timer)
+            }
+        },1000)
+    } else {
+        // 작동중일 때
+    }
 
-    let time = 180
-
-    setInterval(function(){
-        if(time >= 0){
-            let min = Math.floor(time / 60)
-            let sec = String(time % 60).padStart(2, "0")
-            document.getElementById("timer").innerText = min + ":" + sec
-            time = time - 1
-        }
-        else {
-            document.getElementById("finish").disabled = True
-        }
-    },1000)
 }
